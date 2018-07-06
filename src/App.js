3e357bf11187b6735225edf072da0b3a4850417a
Brandon Griffith <BrandonGri5th@gmail.com>
2018-07-06 10:01:17 -0600

    working Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Auth from './component/Auth/Auth';
+import Nav from './component/Nav/Nav';
+import Dashboard from './component/Dashboard/Dashboard';
+import Form from './component/Form/Form';
+import Post from './component/Post/Post';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Nav/>
+        <Auth/>
+        <Dashboard/>
+        <Form/>
+        <Post/>
+
       </div>
     );
   }
